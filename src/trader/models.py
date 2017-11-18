c33c417c3394cadb902b8fbafbefaa28a156078c
Emerson Estrella <emerson.estrella@gmail.com>
2017-11-18 23:20:26 +0100

    CHANGE: Database models increasing tweet size and adding fee and type to trades

--- src/trader/models.py
+++ src/trader/models.py
@@ -6,15 +6,23 @@ class Advice(models.Model):
     position = models.CharField(max_length=30)
     diff = models.FloatField(default=0)
     price = models.FloatField()
-    tweet = models.CharField(max_length=140)
+    tweet = models.CharField(max_length=280)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Trade(models.Model):
+    BUY = 'BUY'
+    SELL = 'SELL'
+    TRADE_TYPES = (
+        (BUY, 'Buy'),
+        (SELL, 'Sell'),
+    )
+
     amount = models.FloatField()
     price = models.FloatField()
     total = models.FloatField()
-    tweet = models.CharField(max_length=140)
+    fee = models.FloatField()
+    type = models.CharField(choices=TRADE_TYPES, max_length=25)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
